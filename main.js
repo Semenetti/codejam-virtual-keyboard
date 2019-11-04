08a2e2b8d48cddab34ffecada52412ad16d1e502
Semenetti <adriano.semenetti@gmail.com>
2019-11-04 21:44:35 +0300

    fix: add obj in arrays; replace a lot of if/else with one for+function; add focus on input when keydown; refactor

--- main.js
+++ main.js
@@ -1,257 +1,254 @@
-let keysRow1En = ['1','2','3','4','5','6','7','8','9','0','Backspace'],    
-    keysRow1Value = ['1','2','3','4','5','6','7','8','9','0','Backspace'],    
-    keysRow1ValueRus = ['1','2','3','4','5','6','7','8','9','0','Backspace'],    
-    keysRow2En = ['Tab','<span>Q</span><sup>Й</sup>','<span>W</span><sup>Ц</sup>','<span>E</span><sup>У</sup>','<span>R</span><sup>К</sup>','<span>T</span><sup>E</sup>','<span>Y</span><sup>Н</sup>','<span>U</span><sup>Г</sup>','<span>I</span><sup>Ш</sup>','<span>O</span><sup>Щ</sup>','<span>P</span><sup>З</sup>'],
-    keysRow2Value = ['Tab','Q','W','E','R','T','Y','U','I','O','P'],
-    keysRow2ValueRus = ['Tab','Й','Ц','У','К','Е','Н','Г','Ш','Щ','З'],
-    keysRow3En = ['Capslock','<span>A</span><sup>Ф</sup>','<span>S</span><sup>Ы</sup>','<span>D</span><sup>В</sup>','<span>F</span><sup>А</sup>','<span>G</span><sup>П</sup>','<span>H</span><sup>Р</sup>','<span>J</span><sup>О</sup>','<span>K</span><sup>Л</sup>','<span>L</span><sup>Д</sup>','Enter'],
-    keysRow3Value = ['CapsLock','A','S','D','F','G','H','J','K','L','Enter'],
-    keysRow3ValueRus = ['Tab','Ф','Ы','В','А','П','Р','О','Л','Д','Enter'],
-    keysRow4En = ['ShiftLeft','<span>Z</span><sup>Я</sup>','<span>X</span><sup>Ч</sup>','<span>C</span><sup>С</sup>','<span>V</span><sup>М</sup>','<span>B</span><sup>И</sup>','<span>N</span><sup>Т</sup>','<span>M</span><sup>Ь</sup>','<span>,</span><sup>Б</sup>','<span>.</span><sup>Ю</sup>','ShiftRight'],
-    keysRow4Value = ['ShiftLeft','Z','X','C','V','B','N','M',',','.','ShiftRight'],
-    keysRow4ValueRus = ['ShiftLeft','Я','Ч','С','М','И','Т','Ь','Б','Ю','ShiftRight'],
-    keysRow5En = ['ControlLeft','AltLeft','Space', 'AltRight', 'ControlRight'],
-    keysRow5Value = ['ControlLeft','AltLeft','Space', 'AltRight', 'ControlRight'];
-    keysRow5ValueRus = ['ControlLeft','AltLeft','Space', 'AltRight', 'ControlRight'];
-    
-let wrapper = document.createElement('div');
+const row1Keys = [{
+  value: '1', valueR: '1', id: 'Digit1', innerHTML: '1',
+}, {
+  value: '2', valueR: '2', id: 'Digit2', innerHTML: '2',
+}, {
+  value: '3', valueR: '3', id: 'Digit3', innerHTML: '3',
+}, {
+  value: '4', valueR: '4', id: 'Digit4', innerHTML: '4',
+}, {
+  value: '5', valueR: '5', id: 'Digit5', innerHTML: '5',
+}, {
+  value: '6', valueR: '6', id: 'Digit6', innerHTML: '6',
+}, {
+  value: '7', valueR: '7', id: 'Digit7', innerHTML: '7',
+}, {
+  value: '8', valueR: '8', id: 'Digit8', innerHTML: '8',
+}, {
+  value: '9', valueR: '9', id: 'Digit9', innerHTML: '9',
+}, {
+  value: '0', valueR: '0', id: 'Digit0', innerHTML: '0',
+}, {
+  value: '', valueR: '', id: 'Backspace', innerHTML: 'Backspace',
+}];
+const row2Keys = [{
+  value: '     ', valueR: '     ', id: 'Tab', innerHTML: 'Tab',
+}, {
+  value: 'Q', valueR: 'Й', id: 'KeyQ', innerHTML: '<span>Q</span><sup>Й</sup>',
+}, {
+  value: 'W', valueR: 'Ц', id: 'KeyW', innerHTML: '<span>W</span><sup>У</sup>',
+}, {
+  value: 'E', valueR: 'У', id: 'KeyE', innerHTML: '<span>E</span><sup>У</sup>',
+}, {
+  value: 'R', valueR: 'К', id: 'KeyR', innerHTML: '<span>R</span><sup>К</sup>',
+}, {
+  value: 'T', valueR: 'Е', id: 'KeyT', innerHTML: '<span>T</span><sup>Е</sup>',
+}, {
+  value: 'Y', valueR: 'Н', id: 'KeyY', innerHTML: '<span>Y</span><sup>Н</sup>',
+}, {
+  value: 'U', valueR: 'Г', id: 'KeyU', innerHTML: '<span>U</span><sup>Г</sup>',
+}, {
+  value: 'I', valueR: 'Ш', id: 'KeyI', innerHTML: '<span>I</span><sup>Ш</sup>',
+}, {
+  value: 'O', valueR: 'Щ', id: 'KeyO', innerHTML: '<span>O</span><sup>Щ</sup>',
+}, {
+  value: 'P', valueR: 'З', id: 'KeyP', innerHTML: '<span>P</span><sup>З</sup>',
+}];
+const row3Keys = [{
+  value: '', valueR: '', id: 'CapsLock', innerHTML: 'CapsLock',
+}, {
+  value: 'A', valueR: 'Ф', id: 'KeyA', innerHTML: '<span>A</span><sup>Ф</sup>',
+}, {
+  value: 'S', valueR: 'Ы', id: 'KeyS', innerHTML: '<span>S</span><sup>Ы</sup>',
+}, {
+  value: 'D', valueR: 'В', id: 'KeyD', innerHTML: '<span>D</span><sup>В</sup>',
+}, {
+  value: 'F', valueR: 'А', id: 'KeyF', innerHTML: '<span>F</span><sup>А</sup>',
+}, {
+  value: 'G', valueR: 'П', id: 'KeyG', innerHTML: '<span>G</span><sup>П</sup>',
+}, {
+  value: 'H', valueR: 'Р', id: 'KeyH', innerHTML: '<span>H</span><sup>Р</sup>',
+}, {
+  value: 'J', valueR: 'О', id: 'KeyJ', innerHTML: '<span>J</span><sup>О</sup>',
+}, {
+  value: 'K', valueR: 'Л', id: 'KeyK', innerHTML: '<span>K</span><sup>Л</sup>',
+}, {
+  value: 'L', valueR: 'Д', id: 'KeyL', innerHTML: '<span>L</span><sup>Д</sup>',
+}, {
+  value: '', valueR: '', id: 'Enter', innerHTML: 'Enter',
+}];
+const row4Keys = [{
+  value: '', valueR: '', id: 'ShiftLeft', innerHTML: 'Shift',
+}, {
+  value: 'Z', valueR: 'Я', id: 'KeyZ', innerHTML: '<span>Z</span><sup>Я</sup>',
+}, {
+  value: 'X', valueR: 'Ч', id: 'KeyX', innerHTML: '<span>X</span><sup>Ч</sup>',
+}, {
+  value: 'C', valueR: 'С', id: 'KeyC', innerHTML: '<span>C</span><sup>С</sup>',
+}, {
+  value: 'V', valueR: 'М', id: 'KeyV', innerHTML: '<span>V</span><sup>М</sup>',
+}, {
+  value: 'B', valueR: 'И', id: 'KeyB', innerHTML: '<span>B</span><sup>И</sup>',
+}, {
+  value: 'N', valueR: 'Т', id: 'KeyN', innerHTML: '<span>N</span><sup>Т</sup>',
+}, {
+  value: 'M', valueR: 'Ь', id: 'KeyM', innerHTML: '<span>M</span><sup>Ь</sup>',
+}, {
+  value: ',', valueR: 'Б', id: 'Comma', innerHTML: '<span>,</span><sup>Б</sup>',
+}, {
+  value: '.', valueR: 'Ю', id: 'Period', innerHTML: '<span>.</span><sup>Ю</sup>',
+}, {
+  value: '', valueR: '', id: 'ShiftRight', innerHTML: 'Shift',
+}];
+const row5Keys = [{
+  value: '', valueR: '', id: 'ControlLeft', innerHTML: 'Ctrl',
+}, {
+  value: '', valueR: '', id: 'AltLeft', innerHTML: 'Alt',
+}, {
+  value: ' ', valueR: ' ', id: 'Space', innerHTML: '',
+}, {
+  value: '', valueR: '', id: 'AltRight', innerHTML: 'Alt',
+}, {
+  value: '', valueR: '', id: 'ControlRight', innerHTML: 'Ctrl',
+}];
+
+let capsLock = true;
+let lang = true;
+
+const wrapper = document.createElement('div');
 wrapper.className = 'wrapper';
 document.body.append(wrapper);
 
-let input = document.createElement('textarea');
+const input = document.createElement('textarea');
 input.setAttribute('id', 'result');
 input.setAttribute('rows', '5');
 input.setAttribute('autofocus', '');
 wrapper.append(input);
 
-let keyboard = document.createElement('div');
+const keyboard = document.createElement('div');
 keyboard.setAttribute('id', 'keyboard');
 wrapper.append(keyboard);
 
-for (let i = 1; i <= 5; i++) {
-    let row = document.createElement('div');
-    row.setAttribute('id', 'row' + i);    
-    keyboard.append(row);
+function createRow(rowNumber, rowKeys) {
+  const row = document.createElement('div');
+  row.setAttribute('id', `row${rowNumber}`);
+  keyboard.append(row);
+  for (let i = 0; i < rowKeys.length; i += 1) {
+    const key = document.createElement('div');
+    key.className = 'key';
+    key.setAttribute('id', rowKeys[i].id);
+    key.setAttribute('value', rowKeys[i].value);
+    key.setAttribute('valueR', rowKeys[i].valueR);
+    key.innerHTML = rowKeys[i].innerHTML;
+    row.append(key);
+  }
 }
+createRow(1, row1Keys);
+createRow(2, row2Keys);
+createRow(3, row3Keys);
+createRow(4, row4Keys);
+createRow(5, row5Keys);
 
-fillKey(keysRow1En, row1);
-fillKey(keysRow2En, row2, keysRow2Value, keysRow2ValueRus);
-fillKey(keysRow3En, row3, keysRow3Value, keysRow3ValueRus);
-fillKey(keysRow4En, row4, keysRow4Value, keysRow4ValueRus);
-fillKey(keysRow5En, row5, keysRow5Value);
-
-function fillKey(x,y,z,v) {
-    for (let i = 0; i < x.length; i++) {
-        let key = document.createElement('div');
-        key.className = 'key';
-        if (x[i] == 'Space') {
-           key.innerHTML = ' ';
-           key.setAttribute('id', 'Space');
-           key.setAttribute('value', ' ')
-        } else if (x[i] == 'Backspace') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '');
-            key.setAttribute('id', 'Backspace');
-        } else if (x[i] == 'Capslock') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '');
-            key.setAttribute('id', 'CapsLock');
-        } else if (x[i] == 'Enter') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '');
-            key.setAttribute('id', 'Enter'); 
-        } else if (x[i] == 'ShiftLeft') {
-            key.innerHTML = 'Shift';
-            key.setAttribute('value', '');
-            key.setAttribute('id', x[i]);
-        } else if (x[i] == 'ShiftRight') {
-            key.innerHTML = 'Shift';
-            key.setAttribute('value', '');
-            key.setAttribute('id', 'ShiftRight');
-        }  else if (x[i] == 'ControlLeft') {
-            key.innerHTML = 'Ctrl';
-            key.setAttribute('value', '');
-            key.setAttribute('id', 'ControlLeft');
-        } else if (x[i] == 'ControlRight') {
-            key.innerHTML = 'Ctrl';
-            key.setAttribute('value', '');
-            key.setAttribute('id', x[i]);
-        } else if (x[i] == 'AltLeft') {
-            key.innerHTML = 'Alt';
-            key.setAttribute('value', '');
-            key.setAttribute('id', 'AltLeft');
-        }  else if (x[i] == 'AltRight') {
-            key.innerHTML = 'Alt';
-            key.setAttribute('value', '');
-            key.setAttribute('id', x[i]);
-        }  else if (x[i] == 'Tab') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '       ');
-            key.setAttribute('id', 'Tab');
-        }  else if (x[i] == '0') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '0');
-            key.setAttribute('id', 'Digit0');
-        }  else if (x[i] == '1') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '1');
-            key.setAttribute('id', 'Digit1');
-        }  else if (x[i] == '2') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '2');
-            key.setAttribute('id', 'Digit2');
-        }  else if (x[i] == '3') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '3');
-            key.setAttribute('id', 'Digit3');
-        }  else if (x[i] == '4') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '4');
-            key.setAttribute('id', 'Digit4');
-        }  else if (x[i] == '5') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '5');
-            key.setAttribute('id', 'Digit5');
-        }  else if (x[i] == '6') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '6');
-            key.setAttribute('id', 'Digit6');
-        }  else if (x[i] == '7') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '7');
-            key.setAttribute('id', 'Digit7');
-        }  else if (x[i] == '8') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '8');
-            key.setAttribute('id', 'Digit8');
-        }  else if (x[i] == '9') {
-            key.innerHTML = x[i];
-            key.setAttribute('value', '9');
-            key.setAttribute('id', 'Digit9');
-        }  else {
-            key.setAttribute('id', 'Key'+z[i]);
-            key.setAttribute('value', z[i]);
-            key.setAttribute('valueR', v[i]);
-            key.innerHTML = x[i];
-        };       
-        y.append(key);         
-    } 
-}
-
-document.querySelectorAll(".key").forEach(element => {    
-    element.addEventListener("click", () => {
-        switch (element.id) {
-            case 'Backspace':
-                document.getElementById('result').value = document.getElementById('result').value.substr(0,document.getElementById('result').value.length-1);
-                break;
-            case 'Enter':
-                document.getElementById('result').value += '\n';
-                break;
-            default:
-                if (lang) {
-                capsLock ? 
-                document.getElementById('result').value += element.getAttribute('value').toUpperCase() : 
-                document.getElementById('result').value += element.getAttribute('value').toLowerCase()  
-                } else {
-                
-                if (capsLock) {
-                    if (element.getAttribute('valueR') == null) {
-                        document.getElementById('result').value += element.getAttribute('value');
-                    } else {
-                        document.getElementById('result').value += element.getAttribute('valueR').toUpperCase();
-                    }
-                } else {
-                    if (element.getAttribute('valueR') == null) {
-                        document.getElementById('result').value += element.getAttribute('value');
-                    } else {
-                        document.getElementById('result').value += element.getAttribute('valueR').toLowerCase();
-                    }                    
-                }                
-                }
-                break;
-        }
-    });
-});
-
-let capsLock = true;
-CapsLock.onclick = function() {
-    upperLower();
-}   
-
-document.addEventListener('keydown', function(event) {
-    if (event.code == 'CapsLock') {
-        upperLower();  
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('sup').forEach((element) => {
+    element.classList.add('lang_reset');
+  });
 });
 
 function upperLower() {
-    capsLock = !capsLock;
-    if (capsLock) {
-        document.querySelectorAll("span").forEach(element => {            
-                element.textContent = element.textContent.toUpperCase();              
-        });
-        document.querySelectorAll("sup").forEach(element => {            
-            element.textContent = element.textContent.toUpperCase();                 
+  capsLock = !capsLock;
+  if (capsLock) {
+    document.querySelectorAll('span').forEach((element) => {
+      element.classList.add('uppercase');
+      element.classList.remove('lowercase');
     });
-    } else {
-        document.querySelectorAll("span").forEach(element => {            
-            element.textContent = element.textContent.toLowerCase();           
+    document.querySelectorAll('sup').forEach((element) => {
+      element.classList.add('uppercase');
+      element.classList.remove('lowercase');
     });
-    document.querySelectorAll("sup").forEach(element => {            
-        element.textContent = element.textContent.toLowerCase();           
+  } else {
+    document.querySelectorAll('span').forEach((element) => {
+      element.classList.add('lowercase');
+      element.classList.remove('uppercase');
     });
-    }
-}
-
-  function runOnKeys(func, ...codes) {
-    let pressed = new Set();
-
-    document.addEventListener('keydown', function(event) {
-      pressed.add(event.code);     
-      document.getElementById(event.code).style.background='#f9de53';
-
-      for (let code of codes) { 
-        console.log(code)
-        if (!pressed.has(code)) {
-          return;
-        }
-      }
-      pressed.clear();
-      func();
-    });
-
-    document.addEventListener('keyup', function(event) {
-    document.getElementById(event.code).style.background='';
-    pressed.delete(event.code);
+    document.querySelectorAll('sup').forEach((element) => {
+      element.classList.add('lowercase');
+      element.classList.remove('uppercase');
     });
   }
-  let lang = true;
-  runOnKeys(
-    () => {
-        lang = !lang;
+}
+
+document.querySelectorAll('.key').forEach((element) => {
+  element.addEventListener('click', () => {
+    switch (element.id) {
+      case 'Backspace':
+        document.getElementById('result').value = document.getElementById('result').value.substr(0, document.getElementById('result').value.length - 1);
+        break;
+      case 'Enter':
+        document.getElementById('result').value += '\n';
+        break;
+      case 'CapsLock':
+        upperLower();
+        break;
+      default:
         if (lang) {
-            document.querySelectorAll("span").forEach(element => {            
-            element.style.fontWeight="900";
-            element.style.fontSize="14px";
-
-            });
-            document.querySelectorAll("sup").forEach(element => {            
-            element.style.fontWeight="400";
-            element.style.fontSize="10px";                 
-             });
+          capsLock
+            ? document.getElementById('result').value += element.getAttribute('value').toUpperCase()
+            : document.getElementById('result').value += element.getAttribute('value').toLowerCase();
+        } else if (capsLock) {
+          if (element.getAttribute('valueR') == null) {
+            document.getElementById('result').value += element.getAttribute('value');
+          } else {
+            document.getElementById('result').value += element.getAttribute('valueR').toUpperCase();
+          }
+        } else if (element.getAttribute('valueR') == null) {
+          document.getElementById('result').value += element.getAttribute('value');
         } else {
-            document.querySelectorAll("span").forEach(element => {            
-            element.style.fontWeight="400";
-            element.style.fontSize="10px";                    
-            });
-            document.querySelectorAll("sup").forEach(element => {            
-            element.style.fontWeight="900";                     
-            element.style.fontSize="14px";
-            });
+          document.getElementById('result').value += element.getAttribute('valueR').toLowerCase();
         }
-    },
-    "AltLeft",
-    "ShiftLeft", 
-    );
+        break;
+    }
+  });
+});
 
- 
+
+function runOnKeys(func, ...codes) {
+  const pressed = new Set();
+
+  document.addEventListener('keydown', (event) => {
+    document.getElementById('result').focus();
+    pressed.add(event.code);
+    if (event.code === 'CapsLock') {
+      upperLower();
+    }
+    document.getElementById(event.code).classList.remove('keyup');
+    document.getElementById(event.code).classList.add('keydown');
+
+    for (const code of codes) {
+      if (!pressed.has(code)) {
+        return;
+      }
+    }
+    pressed.clear();
+    func();
+  });
+
+  document.addEventListener('keyup', (event) => {
+    document.getElementById(event.code).classList.remove('keydown');
+    document.getElementById(event.code).classList.add('keyup');
+    pressed.delete(event.code);
+  });
+}
+
+runOnKeys(
+  () => {
+    lang = !lang;
+    if (lang) {
+      document.querySelectorAll('span').forEach((element) => {
+        element.classList.remove('lang_reset');
+      });
+      document.querySelectorAll('sup').forEach((element) => {
+        element.classList.add('lang_reset');
+      });
+    } else {
+      document.querySelectorAll('span').forEach((element) => {
+        element.classList.add('lang_reset');
+      });
+      document.querySelectorAll('sup').forEach((element) => {
+        element.classList.remove('lang_reset');
+      });
+    }
+  },
+  'AltLeft',
+  'ShiftLeft',
+);
