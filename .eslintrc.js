28783f9f11f35638f257f2ce6e8b4fa1fbf1d456
Nastassia Bulak <nastassia_bulak@epam.com>
2019-11-17 13:47:44 +0300

    fix: eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
-    "extends": "airbnb",
+    "extends": ["eslint:recommended", "eslint-config-airbnb-base"],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -13,5 +12,6 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
+        "linebreak-style": ['error', 'windows']
     }
 };
