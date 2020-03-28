3043271130c99193b8e1dc13b9d90b23cc943661
Semenetti <adriano.semenetti@gmail.com>
2020-03-28 20:38:48 +0300

    feat: cancel commad keys default actions, refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": ["eslint:recommended", "eslint-config-airbnb-base"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "linebreak-style": ['error', 'windows']
-    }
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "eslint-config-airbnb-base"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "linebreak-style": ["error", "windows"],
+    "no-console": "off",
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector:
+          "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        message: "Unexpected property on console object was called"
+      }
+    ]
+  }
 };
--- main.js
+++ main.js
@@ -1,257 +1,315 @@
-
-const row1Keys = [{
-  value: '1',
-  valueRus: '1',
-  id: 'Digit1',
-  innerHTML: '1',
-}, {
-  value: '2',
-  valueRus: '2',
-  id: 'Digit2',
-  innerHTML: '2',
-}, {
-  value: '3',
-  valueRus: '3',
-  id: 'Digit3',
-  innerHTML: '3',
-}, {
-  value: '4',
-  valueRus: '4',
-  id: 'Digit4',
-  innerHTML: '4',
-}, {
-  value: '5',
-  valueRus: '5',
-  id: 'Digit5',
-  innerHTML: '5',
-}, {
-  value: '6',
-  valueRus: '6',
-  id: 'Digit6',
-  innerHTML: '6',
-}, {
-  value: '7',
-  valueRus: '7',
-  id: 'Digit7',
-  innerHTML: '7',
-}, {
-  value: '8',
-  valueRus: '8',
-  id: 'Digit8',
-  innerHTML: '8',
-}, {
-  value: '9',
-  valueRus: '9',
-  id: 'Digit9',
-  innerHTML: '9',
-}, {
-  value: '0',
-  valueRus: '0',
-  id: 'Digit0',
-  innerHTML: '0',
-}, {
-  value: '',
-  valueRus: '',
-  id: 'Backspace',
-  innerHTML: 'Backspace',
-}];
-const row2Keys = [{
-  value: '     ',
-  valueRus: '     ',
-  id: 'Tab',
-  innerHTML: 'Tab',
-}, {
-  value: 'Q',
-  valueRus: 'Й',
-  id: 'KeyQ',
-  innerHTML: '<span>Q</span><sup>Й</sup>',
-}, {
-  value: 'W',
-  valueRus: 'Ц',
-  id: 'KeyW',
-  innerHTML: '<span>W</span><sup>У</sup>',
-}, {
-  value: 'E',
-  valueRus: 'У',
-  id: 'KeyE',
-  innerHTML: '<span>E</span><sup>У</sup>',
-}, {
-  value: 'R',
-  valueRus: 'К',
-  id: 'KeyR',
-  innerHTML: '<span>R</span><sup>К</sup>',
-}, {
-  value: 'T',
-  valueRus: 'Е',
-  id: 'KeyT',
-  innerHTML: '<span>T</span><sup>Е</sup>',
-}, {
-  value: 'Y',
-  valueRus: 'Н',
-  id: 'KeyY',
-  innerHTML: '<span>Y</span><sup>Н</sup>',
-}, {
-  value: 'U',
-  valueRus: 'Г',
-  id: 'KeyU',
-  innerHTML: '<span>U</span><sup>Г</sup>',
-}, {
-  value: 'I',
-  valueRus: 'Ш',
-  id: 'KeyI',
-  innerHTML: '<span>I</span><sup>Ш</sup>',
-}, {
-  value: 'O',
-  valueRus: 'Щ',
-  id: 'KeyO',
-  innerHTML: '<span>O</span><sup>Щ</sup>',
-}, {
-  value: 'P',
-  valueRus: 'З',
-  id: 'KeyP',
-  innerHTML: '<span>P</span><sup>З</sup>',
-}];
-const row3Keys = [{
-  value: '',
-  valueRus: '',
-  id: 'CapsLock',
-  innerHTML: 'CapsLock',
-}, {
-  value: 'A',
-  valueRus: 'Ф',
-  id: 'KeyA',
-  innerHTML: '<span>A</span><sup>Ф</sup>',
-}, {
-  value: 'S',
-  valueRus: 'Ы',
-  id: 'KeyS',
-  innerHTML: '<span>S</span><sup>Ы</sup>',
-}, {
-  value: 'D',
-  valueRus: 'В',
-  id: 'KeyD',
-  innerHTML: '<span>D</span><sup>В</sup>',
-}, {
-  value: 'F',
-  valueRus: 'А',
-  id: 'KeyF',
-  innerHTML: '<span>F</span><sup>А</sup>',
-}, {
-  value: 'G',
-  valueRus: 'П',
-  id: 'KeyG',
-  innerHTML: '<span>G</span><sup>П</sup>',
-}, {
-  value: 'H',
-  valueRus: 'Р',
-  id: 'KeyH',
-  innerHTML: '<span>H</span><sup>Р</sup>',
-}, {
-  value: 'J',
-  valueRus: 'О',
-  id: 'KeyJ',
-  innerHTML: '<span>J</span><sup>О</sup>',
-}, {
-  value: 'K',
-  valueRus: 'Л',
-  id: 'KeyK',
-  innerHTML: '<span>K</span><sup>Л</sup>',
-}, {
-  value: 'L',
-  valueRus: 'Д',
-  id: 'KeyL',
-  innerHTML: '<span>L</span><sup>Д</sup>',
-}, {
-  value: '',
-  valueRus: '',
-  id: 'Enter',
-  innerHTML: 'Enter',
-}];
-const row4Keys = [{
-  value: '',
-  valueRus: '',
-  id: 'ShiftLeft',
-  innerHTML: 'Shift',
-}, {
-  value: 'Z',
-  valueRus: 'Я',
-  id: 'KeyZ',
-  innerHTML: '<span>Z</span><sup>Я</sup>',
-}, {
-  value: 'X',
-  valueRus: 'Ч',
-  id: 'KeyX',
-  innerHTML: '<span>X</span><sup>Ч</sup>',
-}, {
-  value: 'C',
-  valueRus: 'С',
-  id: 'KeyC',
-  innerHTML: '<span>C</span><sup>С</sup>',
-}, {
-  value: 'V',
-  valueRus: 'М',
-  id: 'KeyV',
-  innerHTML: '<span>V</span><sup>М</sup>',
-}, {
-  value: 'B',
-  valueRus: 'И',
-  id: 'KeyB',
-  innerHTML: '<span>B</span><sup>И</sup>',
-}, {
-  value: 'N',
-  valueRus: 'Т',
-  id: 'KeyN',
-  innerHTML: '<span>N</span><sup>Т</sup>',
-}, {
-  value: 'M',
-  valueRus: 'Ь',
-  id: 'KeyM',
-  innerHTML: '<span>M</span><sup>Ь</sup>',
-}, {
-  value: ',',
-  valueRus: 'Б',
-  id: 'Comma',
-  innerHTML: '<span>,</span><sup>Б</sup>',
-}, {
-  value: '.',
-  valueRus: 'Ю',
-  id: 'Period',
-  innerHTML: '<span>.</span><sup>Ю</sup>',
-}, {
-  value: '',
-  valueRus: '',
-  id: 'ShiftRight',
-  innerHTML: 'Shift',
-}];
-const row5Keys = [{
-  value: '',
-  valueRus: '',
-  id: 'ControlLeft',
-  innerHTML: 'Ctrl',
-}, {
-  value: '',
-  valueRus: '',
-  id: 'AltLeft',
-  innerHTML: 'Alt',
-}, {
-  value: ' ',
-  valueRus: ' ',
-  id: 'Space',
-  innerHTML: '',
-}, {
-  value: '',
-  valueRus: '',
-  id: 'AltRight',
-  innerHTML: 'Alt',
-}, {
-  value: '',
-  valueRus: '',
-  id: 'ControlRight',
-  innerHTML: 'Ctrl',
-}];
+const row1Keys = [
+  {
+    value: '1',
+    valueRus: '1',
+    id: 'Digit1',
+    innerHTML: '1',
+  },
+  {
+    value: '2',
+    valueRus: '2',
+    id: 'Digit2',
+    innerHTML: '2',
+  },
+  {
+    value: '3',
+    valueRus: '3',
+    id: 'Digit3',
+    innerHTML: '3',
+  },
+  {
+    value: '4',
+    valueRus: '4',
+    id: 'Digit4',
+    innerHTML: '4',
+  },
+  {
+    value: '5',
+    valueRus: '5',
+    id: 'Digit5',
+    innerHTML: '5',
+  },
+  {
+    value: '6',
+    valueRus: '6',
+    id: 'Digit6',
+    innerHTML: '6',
+  },
+  {
+    value: '7',
+    valueRus: '7',
+    id: 'Digit7',
+    innerHTML: '7',
+  },
+  {
+    value: '8',
+    valueRus: '8',
+    id: 'Digit8',
+    innerHTML: '8',
+  },
+  {
+    value: '9',
+    valueRus: '9',
+    id: 'Digit9',
+    innerHTML: '9',
+  },
+  {
+    value: '0',
+    valueRus: '0',
+    id: 'Digit0',
+    innerHTML: '0',
+  },
+  {
+    value: '',
+    valueRus: '',
+    id: 'Backspace',
+    innerHTML: 'Backspace',
+  },
+];
+const row2Keys = [
+  {
+    value: '     ',
+    valueRus: '     ',
+    id: 'Tab',
+    innerHTML: 'Tab',
+  },
+  {
+    value: 'Q',
+    valueRus: 'Й',
+    id: 'KeyQ',
+    innerHTML: '<span>Q</span><sup>Й</sup>',
+  },
+  {
+    value: 'W',
+    valueRus: 'Ц',
+    id: 'KeyW',
+    innerHTML: '<span>W</span><sup>У</sup>',
+  },
+  {
+    value: 'E',
+    valueRus: 'У',
+    id: 'KeyE',
+    innerHTML: '<span>E</span><sup>У</sup>',
+  },
+  {
+    value: 'R',
+    valueRus: 'К',
+    id: 'KeyR',
+    innerHTML: '<span>R</span><sup>К</sup>',
+  },
+  {
+    value: 'T',
+    valueRus: 'Е',
+    id: 'KeyT',
+    innerHTML: '<span>T</span><sup>Е</sup>',
+  },
+  {
+    value: 'Y',
+    valueRus: 'Н',
+    id: 'KeyY',
+    innerHTML: '<span>Y</span><sup>Н</sup>',
+  },
+  {
+    value: 'U',
+    valueRus: 'Г',
+    id: 'KeyU',
+    innerHTML: '<span>U</span><sup>Г</sup>',
+  },
+  {
+    value: 'I',
+    valueRus: 'Ш',
+    id: 'KeyI',
+    innerHTML: '<span>I</span><sup>Ш</sup>',
+  },
+  {
+    value: 'O',
+    valueRus: 'Щ',
+    id: 'KeyO',
+    innerHTML: '<span>O</span><sup>Щ</sup>',
+  },
+  {
+    value: 'P',
+    valueRus: 'З',
+    id: 'KeyP',
+    innerHTML: '<span>P</span><sup>З</sup>',
+  },
+];
+const row3Keys = [
+  {
+    value: '',
+    valueRus: '',
+    id: 'CapsLock',
+    innerHTML: 'CapsLock',
+  },
+  {
+    value: 'A',
+    valueRus: 'Ф',
+    id: 'KeyA',
+    innerHTML: '<span>A</span><sup>Ф</sup>',
+  },
+  {
+    value: 'S',
+    valueRus: 'Ы',
+    id: 'KeyS',
+    innerHTML: '<span>S</span><sup>Ы</sup>',
+  },
+  {
+    value: 'D',
+    valueRus: 'В',
+    id: 'KeyD',
+    innerHTML: '<span>D</span><sup>В</sup>',
+  },
+  {
+    value: 'F',
+    valueRus: 'А',
+    id: 'KeyF',
+    innerHTML: '<span>F</span><sup>А</sup>',
+  },
+  {
+    value: 'G',
+    valueRus: 'П',
+    id: 'KeyG',
+    innerHTML: '<span>G</span><sup>П</sup>',
+  },
+  {
+    value: 'H',
+    valueRus: 'Р',
+    id: 'KeyH',
+    innerHTML: '<span>H</span><sup>Р</sup>',
+  },
+  {
+    value: 'J',
+    valueRus: 'О',
+    id: 'KeyJ',
+    innerHTML: '<span>J</span><sup>О</sup>',
+  },
+  {
+    value: 'K',
+    valueRus: 'Л',
+    id: 'KeyK',
+    innerHTML: '<span>K</span><sup>Л</sup>',
+  },
+  {
+    value: 'L',
+    valueRus: 'Д',
+    id: 'KeyL',
+    innerHTML: '<span>L</span><sup>Д</sup>',
+  },
+  {
+    value: '',
+    valueRus: '',
+    id: 'Enter',
+    innerHTML: 'Enter',
+  },
+];
+const row4Keys = [
+  {
+    value: '',
+    valueRus: '',
+    id: 'ShiftLeft',
+    innerHTML: 'Shift',
+  },
+  {
+    value: 'Z',
+    valueRus: 'Я',
+    id: 'KeyZ',
+    innerHTML: '<span>Z</span><sup>Я</sup>',
+  },
+  {
+    value: 'X',
+    valueRus: 'Ч',
+    id: 'KeyX',
+    innerHTML: '<span>X</span><sup>Ч</sup>',
+  },
+  {
+    value: 'C',
+    valueRus: 'С',
+    id: 'KeyC',
+    innerHTML: '<span>C</span><sup>С</sup>',
+  },
+  {
+    value: 'V',
+    valueRus: 'М',
+    id: 'KeyV',
+    innerHTML: '<span>V</span><sup>М</sup>',
+  },
+  {
+    value: 'B',
+    valueRus: 'И',
+    id: 'KeyB',
+    innerHTML: '<span>B</span><sup>И</sup>',
+  },
+  {
+    value: 'N',
+    valueRus: 'Т',
+    id: 'KeyN',
+    innerHTML: '<span>N</span><sup>Т</sup>',
+  },
+  {
+    value: 'M',
+    valueRus: 'Ь',
+    id: 'KeyM',
+    innerHTML: '<span>M</span><sup>Ь</sup>',
+  },
+  {
+    value: ',',
+    valueRus: 'Б',
+    id: 'Comma',
+    innerHTML: '<span>,</span><sup>Б</sup>',
+  },
+  {
+    value: '.',
+    valueRus: 'Ю',
+    id: 'Period',
+    innerHTML: '<span>.</span><sup>Ю</sup>',
+  },
+  {
+    value: '',
+    valueRus: '',
+    id: 'ShiftRight',
+    innerHTML: 'Shift',
+  },
+];
+const row5Keys = [
+  {
+    value: '',
+    valueRus: '',
+    id: 'ControlLeft',
+    innerHTML: 'Ctrl',
+  },
+  {
+    value: '',
+    valueRus: '',
+    id: 'AltLeft',
+    innerHTML: 'Alt',
+  },
+  {
+    value: ' ',
+    valueRus: ' ',
+    id: 'Space',
+    innerHTML: '',
+  },
+  {
+    value: '',
+    valueRus: '',
+    id: 'AltRight',
+    innerHTML: 'Alt',
+  },
+  {
+    value: '',
+    valueRus: '',
+    id: 'ControlRight',
+    innerHTML: 'Ctrl',
+  },
+];
 
 let capsLock = true;
 let lang = true;
+const firstRow = 1;
+const secondRow = 2;
+const thirdRow = 3;
+const fourthRow = 4;
+const fifthRow = 5;
 
 const wrapper = document.createElement('div');
 wrapper.className = 'wrapper';
@@ -281,11 +339,11 @@ function createRow(rowNumber, rowKeys) {
     row.append(key);
   }
 }
-createRow(1, row1Keys);
-createRow(2, row2Keys);
-createRow(3, row3Keys);
-createRow(4, row4Keys);
-createRow(5, row5Keys);
+createRow(firstRow, row1Keys);
+createRow(secondRow, row2Keys);
+createRow(thirdRow, row3Keys);
+createRow(fourthRow, row4Keys);
+createRow(fifthRow, row5Keys);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('sup').forEach((element) => {
@@ -316,14 +374,43 @@ function upperLower() {
   }
 }
 
+const cancelCommandKeysDefaultActions = (e) => {
+  switch (e.key) {
+    case 'Tab':
+      e.preventDefault();
+      document.getElementById('result').value += '   ';
+      break;
+    case 'Alt':
+      e.preventDefault();
+      document.getElementById('result').focus();
+      break;
+    case 'Control':
+      e.preventDefault();
+      document.getElementById('result').focus();
+      break;
+    default:
+      document.getElementById('result').focus();
+      break;
+  }
+};
+
+document.addEventListener('keydown', cancelCommandKeysDefaultActions);
+
 document.querySelectorAll('.key').forEach((element) => {
-  element.addEventListener('click', () => {
+  element.addEventListener('click', (e) => {
     switch (element.id) {
       case 'Backspace':
-        document.getElementById('result').value = document.getElementById('result').value.substr(0, document.getElementById('result').value.length - 1);
+        document.getElementById('result').value = document
+          .getElementById('result')
+          .value.substr(0, document.getElementById('result').value.length - 1);
         break;
       case 'Enter':
         document.getElementById('result').value += '\n';
+        break;
+      case 'Tab':
+        e.preventDefault();
+        document.getElementById('result').value += '   ';
+        document.getElementById('result').focus();
         break;
       case 'CapsLock':
         upperLower();
@@ -335,20 +422,27 @@ document.querySelectorAll('.key').forEach((element) => {
             : element.getAttribute('value').toLowerCase();
         } else if (capsLock) {
           if (element.getAttribute('valueRus') == null) {
-            document.getElementById('result').value += element.getAttribute('value');
+            document.getElementById('result').value += element.getAttribute(
+              'value',
+            );
           } else {
-            document.getElementById('result').value += element.getAttribute('valueRus').toUpperCase();
+            document.getElementById('result').value += element
+              .getAttribute('valueRus')
+              .toUpperCase();
           }
         } else if (element.getAttribute('valueRus') == null) {
-          document.getElementById('result').value += element.getAttribute('value');
+          document.getElementById('result').value += element.getAttribute(
+            'value',
+          );
         } else {
-          document.getElementById('result').value += element.getAttribute('valueRus').toLowerCase();
+          document.getElementById('result').value += element
+            .getAttribute('valueRus')
+            .toLowerCase();
         }
         break;
     }
   });
 });
-
 
 function runOnKeys(func, ...codes) {
   const pressed = new Set();
@@ -364,8 +458,11 @@ function runOnKeys(func, ...codes) {
       document.getElementById(event.code).classList.add('keydown');
     }
 
-
     if (codes.some((code) => !pressed.has(code))) {
+      document.querySelectorAll('.key').forEach((element) => {
+        element.classList.remove('keyup');
+        // element.classList.add("keydown");
+      });
       return;
     }
 
@@ -373,10 +470,23 @@ function runOnKeys(func, ...codes) {
     func();
   });
 
+  // Clear highlight keys when out of input
+  document.getElementById('result').addEventListener('focusout', () => {
+    document.querySelectorAll('.key').forEach((element) => {
+      element.classList.remove('keydown');
+    });
+  });
+
   document.addEventListener('keyup', (event) => {
-    document.getElementById(event.code).classList.remove('keydown');
-    document.getElementById(event.code).classList.add('keyup');
-    pressed.delete(event.code);
+    try {
+      document.getElementById(event.code).classList.remove('keydown');
+      document.getElementById(event.code).classList.add('keyup');
+      pressed.delete(event.code);
+    } catch (error) {
+      console.warn(
+        `Нажатая клавиша "${event.key}" отсутствует на виртуальной клавиатуре`,
+      );
+    }
   });
 }
 
